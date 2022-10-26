5d32c16e9b4ce971c358f387cb968e41f2ed81f3
Dmitri Tsiu <dmitrimeowsalot@gmail.com>
2022-10-27 00:15:33 +0300

    Added datatable

--- src/main/webapp/resources/js/afterAttempt.js
+++ src/main/webapp/resources/js/afterAttempt.js
@@ -1,14 +1,5 @@
 function afterAttempt(data) {
     if (data.status === 'success') {
-        const els = document.getElementsByClassName('timestamp');
-
-        for (let i = 0; i < els.length; i++) {
-            const el = els.item(i);
-            const timestamp = parseInt(el.innerHTML);
-            if (!isNaN(timestamp)) {
-                el.innerHTML = new Date(timestamp).toLocaleString();
-            }
-        }
         fetch('/web-faces/attempts')
             .then((data) => data.json())
             .then((data) => {
@@ -17,3 +8,17 @@ function afterAttempt(data) {
             });
     }
 }
+
+function renderTimestampsOnTable() {
+    const els = document.getElementsByClassName('timestamp');
+
+    for (let i = 0; i < els.length; i++) {
+        const el = els.item(i);
+        const timestamp = parseInt(el.innerHTML);
+        if (!isNaN(timestamp) && el.innerHTML.match('^[0-9]+$')) {
+            el.innerHTML = new Date(timestamp).toLocaleString();
+        }
+    }
+}
+
+setInterval(renderTimestampsOnTable, 10);
